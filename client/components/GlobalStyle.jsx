57175ed6b7fa6c283440178f516cea827e835fcd
Maks V <maks@nicer.io>
2019-11-17 14:19:39 -0500

    working on gettting data

--- client/components/GlobalStyle.jsx
+++ client/components/GlobalStyle.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import Radium, { Style } from "radium";
+import { Global, css } from "@emotion/core";
 
-export default function GlobalStyle() {
+export const colors = {
+  main: "#fff",
+  light: "#F4F7FD"
+};
+
+export const breakingPoints = {
+  xl: "@media (max-width: 1200px)",
+  lg: "@media (max-width: 1000px)",
+  md: "@media (max-width: 800px)",
+  sm: "@media (max-width: 600px)"
+};
+
+export default function Styles() {
   return (
-    <Style
-      rules={{
-        body: {
-          backgroundColor: "#F4F7FD",
-          margin: 0,
-          padding: 0,
-          fontFamily: "Roboto",
-          boxSizing: "border-box"
+    <Global
+      styles={css`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
         }
-      }}
+
+        body {
+          background-color: "#F4F7FD";
+          margin: 0;
+          padding: 0;
+          font-family: "Roboto", sans-serif;
+        }
+      `}
     />
   );
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,61 +1,66 @@
-import React from "react";
-import firebase from "firebase";
-import { Input, TextField, Typography } from "@material-ui/core";
+import React from "react"
+import firebase from "firebase"
+import { Input, TextField, Typography } from "@material-ui/core"
 
-function Index({ ...rest }) {
-  console.log(rest);
+function Index({ categoriesQuery, ...rest }) {
+  console.log(rest)
   return (
     <div>
       <section
-        style={{
+        css={{
           maxWidth: 1000,
           margin: "50px auto",
           display: "flex",
           alignItems: "center",
           backgroundColor: "#BCD4FD",
           padding: "20px 40px",
-          borderRadius: 30
-        }}
-      >
-        <div style={{ width: "50%", margin: "50px 0" }}>
+          borderRadius: 30,
+        }}>
+        <div css={{ width: "50%", margin: "50px 0" }}>
           <h1
-            style={{
+            css={{
               margin: "0 0 30px",
               fontWeight: "lighter",
-              fontSize: 35
-            }}
-          >
+              fontSize: 35,
+            }}>
             The Easiest Place to Get <strong>Worship Songs</strong>
           </h1>
           <TextField
             color="secondary"
-            style={{ width: "60%" }}
+            css={{ width: "60%" }}
             label="Search"
             variant="outlined"
           />
         </div>
-        <div style={{ width: "50%" }}>
+        <div css={{ width: "50%" }}>
           {/* <img style={{ width: "80%" }} src="/static/landing.svg" /> */}
         </div>
       </section>
-      <section style={{ maxWidth: 1000, margin: "50px auto" }}>
-        <Typography variant="h5" component="h3">
+      <section css={{ maxWidth: 1000, margin: "50px auto" }}>
+        <Typography css={{ marginLeft: 40 }} variant="h5" component="h3">
           Categories
         </Typography>
         <div></div>
       </section>
     </div>
-  );
+  )
 }
 
-export default Index;
+export default Index
 
 Index.getInitialProps = async ({ query }) => {
-  // const categoriesQuery = await firebase
-  //   .firestore()
-  //   .collection("categories")
-  //   .get();
-  console.log("ran");
+  let categories = []
+  firebase
+    .firestore()
+    .collection("categories")
+    .get()
+    .then(res => {
+      console.log(res.size)
+      res.docs.forEach(doc => {
+        const data = doc.data()
+        categories.push({ ...data, id: doc.id })
+      })
+    })
 
-  return { test: "test" };
-};
+  return { categories }
+}
