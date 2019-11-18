6836c2270469f53f3ed50e6343c7852fe8ebae44
Maks V <maks@nicer.io>
2019-11-17 20:30:51 -0500

    working on uploading

--- upload.js
+++ upload.js
@@ -1,6 +1,8 @@
-const firebase = require("firebase");
-const csv = require("csv-parser");
-const fs = require("fs");
+const firebase = require("firebase")
+const csv = require("csv-parser")
+const path = require("path")
+const fs = require("fs")
+const officeParser = require("officeparser")
 
 const firebaseConfig = {
   apiKey: "AIzaSyCajsm4n3CDl0hlK3Tx7A9-ovxCB7AKJzo",
@@ -10,43 +12,61 @@ const firebaseConfig = {
   storageBucket: "song-spot-e31ef.appspot.com",
   messagingSenderId: "1043510911013",
   appId: "1:1043510911013:web:a4f29933a37efd13c71fcc",
-  measurementId: "G-RC5JG4KHBP"
-};
+  measurementId: "G-RC5JG4KHBP",
+}
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
 }
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
-    await callback(array[index], index, array);
+    await callback(array[index], index, array)
   }
 }
 
 async function main() {
-  let results = [];
-  fs.createReadStream("hymnen.csv")
+  let results = []
+  fs.createReadStream("hymnru.csv")
     .pipe(csv())
     .on("data", data => results.push(data))
     .on("end", () => {
-      console.log(results[0]);
+      console.log(results[0])
       asyncForEach(results, async (data, index) => {
-        console.log(`${index + 1} / ${results.length}`);
-        const lyrics = data.song_text.replace(/&|Verse \d|Verse|Refrain/g, "");
-        console.log(lyrics);
+        console.log(`${index + 1} / ${results.length}`)
+        // const lyrics = data.song_text.replace(/&|Verse \d|Verse|Refrain/g, "")
+        const lyrics = data.song_text.replace(
+          /&|Куплет \d|Куплет|Припев \d|Припев/g,
+          "",
+        )
         await firebase
           .firestore()
           .collection("songs")
-          .doc("dtlL8PnEBSmjwg5TGEDM")
-          .collection("files")
           .add({
             title: data.title,
             words: data.words,
             number: data.number,
-            lyrics
-          });
-      });
-    });
+            lyrics,
+            lang: "ru",
+            group: "hymn",
+          })
+      })
+    })
 }
 
-main();
+// async function main() {
+//   console.log(__dirname)
+//   const directoryPath = path.join(__dirname, "powerpoints")
+//   fs.readdir(directoryPath, function(err, files) {
+//     console.log("ran", files, directoryPath + "/" + files[1])
+//     asyncForEach(files, async file => {
+//       await officeParser.parsePowerPoint(directoryPath + "/" + file, function(
+//         data,
+//       ) {
+//         console.log(file, data)
+//       })
+//     })
+//   })
+// }
+
+main()
